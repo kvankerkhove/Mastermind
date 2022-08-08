f54ce467f1fe35ed69f234352b90df9147081ca9
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-08-08 16:53:42 -0400

    added hint pegs feature

--- src/components/HintPegs.js
+++ src/components/HintPegs.js
@@ -1,20 +1,42 @@
+import { render } from '@testing-library/react'
 import React from 'react'
 
 function HintPegs({hintPegKey}) {
 
   let rightColorRightSpot = hintPegKey[0]
   let rightColorWrongSpot = hintPegKey[1]
+  let blank = 4 - (rightColorRightSpot + rightColorWrongSpot)
+  
 
   console.log(`RCRS: ${rightColorRightSpot}`)
-  console.log(`RCWS: ${rightColorWrongSpot}  `)
+  console.log(`RCWS: ${rightColorWrongSpot}`)
+  console.log(blank)
+
+  const renderRCRS = [...Array(rightColorRightSpot).keys()].map(pep => {
+    return <div className="check-circle" style={{border: '2px solid black', backgroundColor: 'black'}}></div>
+  })
+  console.log(renderRCRS)
+
+  const renderRCWS = [...Array(rightColorWrongSpot).keys()].map(peg => {
+    return <div className="check-circle" style={{border: '2px solid black', backgroundColor: "white"}}></div>
+    
+  })
+  console.log(renderRCWS)
+
+  const renderBlank = [...Array(blank).keys()].map(peg => {
+    return <div className="check-circle"></div>
+
+  })
+  console.log(renderBlank)
+
+
 
  
   return (
     <div className="check-circles">
-        <div className='check-circle' id='check-circle-1'></div>
-        <div className='check-circle' id='check-circle-2'></div>
-        <div className='check-circle' id='check-circle-3'></div>
-        <div className='check-circle' id='check-circle-4'></div>
+      {renderRCRS}
+      {renderRCWS}
+      {renderBlank}
       </div>
   )
 }
@@ -22,3 +44,8 @@ function HintPegs({hintPegKey}) {
 export default HintPegs
 
 // style={{border: '2px solid black', backgroundColor: 'black'}}
+
+{/* <div className='check-circle' id='check-circle-1'></div>
+  <div className='check-circle' id='check-circle-2'></div>
+  <div className='check-circle' id='check-circle-3'></div>
+  <div className='check-circle' id='check-circle-4'></div> */}
