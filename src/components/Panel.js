0b7de665a8d4c4ab39872ceb38f576d78301abc7
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-09-16 13:46:33 -0400

    modal z-index bug fix

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ColorButton from './ColorButton'
+import './Panel.css'
 
 function Panel({setCurrentColor, level, modal, handleNewGameButton, currentColor}) {
     
@@ -12,7 +13,7 @@ function Panel({setCurrentColor, level, modal, handleNewGameButton, currentColor
         <div id='buttons'>
           {renderButtons}
         </div>
-        <button onClick={() => handleNewGameButton('Would you like to start a new game?', 'Click YES to start a new game and NO to resume current game', 'YES', 'NO')} >new game</button>
+        <button id='new-game-button' onClick={() => handleNewGameButton('Would you like to start a new game?', 'Click YES to start a new game and NO to resume current game', 'YES', 'NO')} >new game</button>
     </div>
   )
 }
