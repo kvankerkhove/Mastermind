f1b7e817ff631614ba81d40e020bebad8e1d768f
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-09-16 12:52:40 -0400

    rules of mastermind

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,12 @@ import './Header.css'
 
 function Header() {
   return (
-    <h1 id='mastermind'>Mastermind</h1>
+    <>
+      <h1 id='mastermind'>Mastermind</h1>
+      <p>The object of MASTERMIND is to guess a secret code consisting of a series of 4 colored circles. Each guess results in feedback narrowing down the possibilities of the code. The winner is the player who solves the secret code in 8 guesses of less.</p>
+      <p>Each time a player guesses 4 colors, click CHECK to reveal feedback. The black peg means that there is a correct color in the correct spot. The white peg means that there is a correct color in the wrong spot. A void peg means a player did not pick a correct color.</p>
+
+    </>
   )
 }
 
