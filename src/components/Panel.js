d25ceb5290e592c3fd501484c0e347b8961c6e8d
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-09-18 11:32:06 -0400

    display radio buttons

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -13,6 +13,26 @@ function Panel({setCurrentColor, level, modal, handleNewGameButton, currentColor
         <div id='buttons'>
           {renderButtons}
         </div>
+        <div id='radio-buttons'>
+          <label class="radio-btn">
+            <input type="checkbox" checked="checked"/>
+            <span class="checkmark"></span>
+            Easy
+          </label>
+
+          <label class="radio-btn">
+            <input type="checkbox"/>
+            <span class="checkmark"></span>
+            Medium
+          </label>
+
+          <label class="radio-btn">
+            <input type="checkbox"/>
+            <span class="checkmark"></span>
+            Hard
+          </label>
+        </div>
+        <br></br>
         <button id='new-game-button' onClick={() => handleNewGameButton('Would you like to start a new game?', 'Click YES to start a new game and NO to resume current game', 'YES', 'NO')} >new game</button>
     </div>
   )
