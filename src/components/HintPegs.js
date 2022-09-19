dcc126c5730a8571df612dc3ea2ef5553f2aed22
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-09-19 12:00:40 -0400

    hint peg shadowing

--- src/components/HintPegs.js
+++ src/components/HintPegs.js
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react'
+// import { render } from '@testing-library/react'
 import React from 'react'
 import './HintPegs.css'
 
@@ -9,17 +9,27 @@ function HintPegs({hintPegKey}) {
   let blank = 4 - (rightColorRightSpot + rightColorWrongSpot)
 
   const renderRCRS = [...Array(rightColorRightSpot).keys()].map(pep => {
-    return <div className="check-circle" style={{border: '2px solid black', backgroundColor: 'black'}}></div>
+    return (
+      <div className='check-circle-container'>
+        <div className="check-circle" style={{height: '1.6vw', width: '1.6vw', backgroundColor: "#605D5D", boxShadow: 'inset -.1vw -.1vw .7vw .1vw black'}}></div>
+      </div>
+    )
   })
 
   const renderRCWS = [...Array(rightColorWrongSpot).keys()].map(peg => {
-    return <div className="check-circle" style={{border: '2px solid white', backgroundColor: "white", boxShadow: 'none'}}></div>
-    
+    return (
+      <div className='check-circle-container'>
+        <div className="check-circle" style={{height: '1.6vw', width: '1.6vw', backgroundColor: "white", boxShadow: 'inset -.1vw -.1vw .5vw .1vw gray'}}></div>
+      </div>
+    )
   })
 
   const renderBlank = [...Array(blank).keys()].map(peg => {
-    return <div className="check-circle"></div>
-
+    return (
+      <div className='check-circle-container'>
+        <div className="check-circle"></div>
+      </div>
+    )
   })
 
   return (
@@ -32,3 +42,5 @@ function HintPegs({hintPegKey}) {
 }
 
 export default HintPegs
+
+// border: '2px solid white'
